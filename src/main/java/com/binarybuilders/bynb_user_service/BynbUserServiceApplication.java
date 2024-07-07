07023103f2895d54005dc356d63a268620812857
Vlad Sarlea <93834439+Dutu06@users.noreply.github.com>
2024-07-07 21:48:26 +0300

    Created the Spring Boot Microservice

---
+++ src/main/java/com/binarybuilders/bynb_user_service/BynbUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.binarybuilders.bynb_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BynbUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BynbUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/binarybuilders/bynb_user_service/BynbUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binarybuilders.bynb_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BynbUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
